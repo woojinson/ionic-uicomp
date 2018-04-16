2330775fed002b5548ee1a22211d593711f94505
woojinson <javajjang@nate.com>
2018-04-16 21:45:38 +0900

    userName 변수 추가

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  
+  userName:any;//변수 선언 : 타입스크립트
+  
+  constructor(public navCtrl: NavController) {//생성자 = 객체 생성시 잴 먼져 호출됨
+    this.userName = "아이오닉";//값을 할 당
   }
 
 }
