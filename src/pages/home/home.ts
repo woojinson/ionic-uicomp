9f7644d1a178f66a918fbab773aa64191b850d3f
woojinson <javajjang@nate.com>
2018-04-17 19:56:21 +0900

    list TEST2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  
+
   userName:any;//변수 선언 : 타입스크립트
-  
+  items = [
+    {"id":1, name:"첫번째 Item"},
+    {"id":2, name:"두번째 Item"},
+    {"id":3, name:"세번째 Item"},
+  ];
+
+  itemSelected(item){
+    alert(item.id);
+  }
+
+  clickButton(event){
+    alert(event.target.textContent)
+    console.log(event.clientX + ","+event.clientY);
+  }
+
   constructor(public navCtrl: NavController) {//생성자 = 객체 생성시 잴 먼져 호출됨
     this.userName = "아이오닉";//값을 할 당
   }
