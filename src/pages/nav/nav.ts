5599509452d6b2b632ce3faf34b04065daf3f0f4
woojinson <javajjang@nate.com>
2018-04-20 21:35:22 +0900

    add LoaderProvider

--- src/pages/nav/nav.ts
+++ src/pages/nav/nav.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {AccountInterface} from "../../interfaces/account";
+import {EmailComposer} from "@ionic-native/email-composer";
 
 /**
  * Generated class for the NavPage page.
@@ -18,12 +19,25 @@ export class NavPage {
 
   private accountData = {} as AccountInterface;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private emailComposer:EmailComposer) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NavPage');
     this.accountData = this.navParams.get("account");
+  }
+
+  sendEmail(account){
+    let email = {
+      to: account.email,
+      subject: 'To : '+account.name,
+      body: '',
+
+
+    }
+    this.emailComposer.open(email);
   }
 
 }
